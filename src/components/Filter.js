e1e79951f73fe007d4fbd330dbf50f29e7235e6a
massqeen <62159695+massqeen@users.noreply.github.com>
2020-09-25 23:00:28 +0300

    fixes styles for the components

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 
 const Filter = ({ value = '', onChange }) => (
   <label>
-    Фильтр по имени
-    <input type="text" value={value} onChange={onChange} />
+    Фильтр по имени:
+    <input type="text" value={value} onChange={onChange} style={{ marginLeft: '1em', marginBottom: '1em' }}/>
   </label>
 );
 Filter.propTypes = {
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,33 +2,38 @@ import React from 'react';
 import classNames from 'classnames';
 import styles from './TodoList.module.css';
 
-const TodoList = ({ todos, onDeleteTodo, onToggleCompleted }) => (
-  <ul className={styles.TodoList}>
-    {todos.map(({ id, text, completed }) => (
-      <li
-        key={id}
-        // TODO заменить на метод для CSS-модулей
-        className={classNames('TodoList__item', {
-          'TodoList__item--completed': completed,
-        })}
-      >
-        <input
-          type="checkbox"
-          className={styles.checkbox}
-          checked={completed}
-          onChange={() => onToggleCompleted(id)}
-        />
-        <p className={styles.text}>{text}</p>
-        <button
-          type="button"
-          className={styles.btn}
-          onClick={() => onDeleteTodo(id)}
-        >
-          Удалить
-        </button>
-      </li>
-    ))}
-  </ul>
-);
+console.log(styles);
+const boundClassNames = classNames.bind(styles);
+const TodoList = ({ todos, onDeleteTodo, onToggleCompleted }) => {
+  return (
+    <ul className={styles.TodoList}>
+      {todos.map(({ id, text, completed }) => {
+        const className = boundClassNames({
+        [styles.item]: true, [styles.completed]: completed });
+        return (
+          <li
+            key={id}
+            className={className}
+          >
+            <input
+              type="checkbox"
+              className={styles.checkbox}
+              checked={completed}
+              onChange={() => onToggleCompleted(id)}
+            />
+            <p className={styles.text}>{text}</p>
+            <button
+              type="button"
+              className={styles.btn}
+              onClick={() => onDeleteTodo(id)}
+            >
+              Удалить
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 export default TodoList;
