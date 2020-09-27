5bb5d91e85a4fbceafb30b3a37942a9b9f0af692
Arthur Plyuschevsky <62159695+massqeen@users.noreply.github.com>
2020-09-27 10:45:55 +0300

    Updates

---
+++ precache-manifest.bd080f186b870d0c7735c37b11165b86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3daea59eecf726cf855ea3644e69c1f",
+    "url": "/goit-react-hw-02-phonenumber/index.html"
+  },
+  {
+    "revision": "b76ddad2c55ad129f32a",
+    "url": "/goit-react-hw-02-phonenumber/static/css/2.9419f0e9.chunk.css"
+  },
+  {
+    "revision": "46fdc8e66d0667bdc735",
+    "url": "/goit-react-hw-02-phonenumber/static/css/main.b4585295.chunk.css"
+  },
+  {
+    "revision": "b76ddad2c55ad129f32a",
+    "url": "/goit-react-hw-02-phonenumber/static/js/2.3e596117.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/goit-react-hw-02-phonenumber/static/js/2.3e596117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46fdc8e66d0667bdc735",
+    "url": "/goit-react-hw-02-phonenumber/static/js/main.b2725925.chunk.js"
+  },
+  {
+    "revision": "dcc233e01c199e13e289",
+    "url": "/goit-react-hw-02-phonenumber/static/js/runtime-main.22383156.js"
+  }
+]);
